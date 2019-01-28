931b8d2af566b8514469b7ba36a4f1d7e46a05d0
oskarwahlqvist <oskar_wahlqvist@hotmail.com>
2019-01-28 09:12:59 +0100

    hej

---
+++ helloworld/input.py
@@ -0,0 +1,10 @@
+
+
+name = input("Please enter your name: ")
+age = input("How old are you? {0}? ".format(name))
+print(age)
+
+
+name = input("Please enter your name: ")
+age = int(input("How old are you? {0}? ".format(name)))
+print(age)
